606fe17d618d053f721948db010fb78203dc666d
jessemonroy650 <jessemonroy650@yahoo.com>
2018-10-02 12:59:21 -0600

    change visual format

--- file.js
+++ file.js
@@ -7,9 +7,9 @@ var filePlugin = {
         fs.root.getFile('index.html', {create: false}, 
             function (fileEntry) {
                 var tempdata = "" +
-                "<p class=r /><b>fileEntry.isFile:</b> "   + fileEntry.isFile +
-                "<p class=g /><b>fileEntry.name:</b> "     + fileEntry.name +
-                "<p class=b /><b>fileEntry.fullPath:</b> " + fileEntry.fullPath;
+                "<br class=r /><b>fileEntry.isFile:</b> "   + fileEntry.isFile +
+                "<br class=g /><b>fileEntry.name:</b> "     + fileEntry.name +
+                "<br class=b /><b>fileEntry.fullPath:</b> " + fileEntry.fullPath;
 
                 document.getElementById('status').innerHTML = tempdata;
             }, 
@@ -23,24 +23,24 @@ var filePlugin = {
     //
     properties2HTML : function (outputElementId) {
         var fileStuff = "" +
-        "<p class=r /><b>cordova.file.applicationDirectory:</b> "                + cordova.file.applicationDirectory +
-        "<p class=g /><b>cordova.file.applicationStorageDirectory:</b> "         + cordova.file.applicationStorageDirectory +
-        "<p class=b /><b>cordova.file.dataDirectory:</b> "                       + cordova.file.dataDirectory +
-        "<p class=a /><b>cordova.file.cacheDirectory:</b> "                      + cordova.file.cacheDirectory +
-        "<p class=p /><b>cordova.file.externalApplicationStorageDirectory:</b> " + cordova.file.externalApplicationStorageDirectory +
-        "<p class=r /><b>cordova.file.externalDataDirectory:</b> "               + cordova.file.externalDataDirectory +
-        "<p class=g /><b>cordova.file.externalCacheDirectory:</b> "              + cordova.file.externalCacheDirectory +
-        "<p class=b /><b>cordova.file.externalRootDirectory:</b> "               + cordova.file.externalRootDirectory; 
+        "<br class=r /><b>cordova.file.applicationDirectory:</b> "                + cordova.file.applicationDirectory +
+        "<br class=g /><b>cordova.file.applicationStorageDirectory:</b> "         + cordova.file.applicationStorageDirectory +
+        "<br class=b /><b>cordova.file.dataDirectory:</b> "                       + cordova.file.dataDirectory +
+        "<br class=a /><b>cordova.file.cacheDirectory:</b> "                      + cordova.file.cacheDirectory +
+        "<br class=p /><b>cordova.file.externalApplicationStorageDirectory:</b> " + cordova.file.externalApplicationStorageDirectory +
+        "<br class=r /><b>cordova.file.externalDataDirectory:</b> "               + cordova.file.externalDataDirectory +
+        "<br class=g /><b>cordova.file.externalCacheDirectory:</b> "              + cordova.file.externalCacheDirectory +
+        "<br class=b /><b>cordova.file.externalRootDirectory:</b> "               + cordova.file.externalRootDirectory; 
 
         document.getElementById(outputElementId).innerHTML = fileStuff;
     },
     //
     aboutFile2HTML : function (fileEntry, feedbackId) {
         var feedbackStuff = "" +
-        "<p class=r /><b>fileEntry.isFile:</b> "      + fileEntry.isFile +
-        "<p class=g /><b>fileEntry.isDirectory:</b> " + fileEntry.isDirectory +
-        "<p class=b /><b>fileEntry.name:</b> "        + fileEntry.name +
-        "<p class=r /><b>fileEntry.fullPath:</b> "    + fileEntry.fullPath;
+        "<br class=r /><b>fileEntry.isFile:</b> "      + fileEntry.isFile +
+        "<br class=g /><b>fileEntry.isDirectory:</b> " + fileEntry.isDirectory +
+        "<br class=b /><b>fileEntry.name:</b> "        + fileEntry.name +
+        "<br class=r /><b>fileEntry.fullPath:</b> "    + fileEntry.fullPath;
 
         document.getElementById(feedbackId).innerHTML = feedbackStuff;
     }
